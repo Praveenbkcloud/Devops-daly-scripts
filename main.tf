7326db9c71df1fb544f22466ce9f4cb19c81aa40
Praveenbkcloud <praveenbkcloud@gmail.com>
2025-02-06 20:55:20 +0530

    changes in the main file

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "workspacedellate"
-    key    = "workspace.tfstate"
-    region = "us-east-1"
+    bucket             = "workspacedellate"
+    key                = "workspace.tfstate"
+    region             = "us-east-1"
+    aws_dynamodb_table = "dynamodb-state-locking"
   }
 }
 
