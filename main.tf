50f632b3d1583ea708f7a6ef190f3225330ae690
Praveenbkcloud <praveenbkcloud@gmail.com>
2025-02-07 15:28:33 +0530

    commented dynamo db and ec2.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "workspacedellate"
-    key            = "workspace.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "dynamodb-state-locking"
+    bucket = "workspacedellate"
+    key    = "workspace.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "dynamodb-state-locking"
   }
 }
 
